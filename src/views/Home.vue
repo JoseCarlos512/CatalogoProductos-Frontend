88673575bbdef9aa1f3a7d9d9e491b392240b01f
JoseCarlos <jose.leon@deister.es>
2021-08-01 20:33:13 -0500

    Primer commit - avance del front desarrollado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card outlined>
+    <v-card-title>
+      Catalogo de Productos
+    </v-card-title>
+    <v-divider/>
+
+
+    <v-card-text>
+        <v-row>
+            <v-col cols='10' sm='12' md='10' >
+              <v-text-field
+                label="Buscar por nombre o descripcion"
+                outlined
+                dense>
+              </v-text-field>
+            </v-col>
+            <v-col cols='2' sm='12' md='2'  >
+              <v-btn color='green' text outlined @click="openForm">
+                Nuevo Producto
+              </v-btn>
+            </v-col>
+        </v-row>
+        <v-row style="margin-top: -30px">
+          <v-col cols="12" md="3" sm="6" v-for="(producto, index) in productos" :key="index">
+              <producto/>
+          </v-col>
+        </v-row >
+    </v-card-text>
+
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  import producto from "../components/producto"
+  export default {
+    name: 'Home',
+    components: {
+      producto
+    },
+    data() {
+      return {
+        productos : [
+          {id : 1},
+          {id : 2},
+          {id : 3},
+          {id : 4},
+          {id : 5},
+          {id : 6},
+          {id : 7},
+          {id : 8},
+          {id : 9},
+          {id : 10},
+          {id : 11},
+          {id : 12}
+        ]
+      }
+    },
+    methods: {
+      openForm() {
+        this.$router.push({name: 'Producto'}); // name: nombre ruta
+      }
+    },
   }
-}
 </script>
---
+++ src/views/producto-form.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card outlined>
+
+    <v-card-title>
+      <h3>Nuevo-Editar Producto</h3>
+
+      <v-space/>
+      <v-btn outlined @click="$router.back()">
+          regresar
+      </v-btn>
+    </v-card-title>
+
+    <v-divider/>
+    
+    <v-card-text>
+      <v-card-actions>
+        <v-btn color="green" outlined>
+            Guardar
+        </v-btn>
+      </v-card-actions>
+    </v-card-text>
+
+  </v-card>
+</template>
+
+
+<!-- Aqui va todo el codigo -->
+<!-- java script o vuejs    -->
+<script>
+export default {
+  name: "producto-form",
+  data() {
+    // Declaracion de variables locales
+    return {
+      nombre: ""
+    }
+  },
+  computed: {
+    // Cambiar el estado o valor de las variables en tiempo de ejecucion.
+    // operaciones de variable
+  },
+  created() {
+    // Se ejecuta antes de dibujar el (DOM  de la pagina = la pagina)
+  },
+  mounted() {
+    // Se ejecuta despues de dibujar el (DOM  de la pagina = la pagina)
+  },
+  methods: {
+    
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
